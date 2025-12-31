9b5712e914dda340d56c3b57025bb2722e29570d
VeeSolstice <vainikapanwarr.21042@gmail.com>
2025-12-31 18:59:02 +0530

    some change

--- Assignment_1.py/Task_2.py
+++ Assignment_1.py/Task_2.py
@@ -8,4 +8,10 @@
 fn = str(input("Enter your first name: "))
 ln = str(input("Enter your last name: "))
 
-print("Hello, ",fn, ln,"! Welcome to the Python program")
+# Concatenate the names to create the full name
+# We add a space " " between the first and last name
+full_name = fn+ " " + ln
+
+# 3. Print the personalized greeting
+# Using an f-string for clean formatting
+print(f"Hello, {full_name}! Welcome to the Python program.")
