47f4f92d04b93752a2ea1669a9556e5b6406f2c7
VeeSolstice <vainikapanwarr.21042@gmail.com>
2026-01-17 20:08:32 +0530

    Add numerical operations examples: min, max, and sum functions

---
+++ Module_5.py/Numerical_Operations.py
@@ -0,0 +1,13 @@
+numbers = [10, 4, -1, 20, 5.5, 7, 1]
+# if munber = [10, 4, -1, 20, 5.5, 7, 1, "Hi"] them TypeError
+# Smallest number in the list
+# min()
+print(f"The biggest number in the list: {min(numbers)}")
+
+# Biggest number in the list
+# max()
+print(f"The smallest number in the list: {max(numbers)}")
+
+# Summation of all the elements in the list
+# sum()
+print(f"The sum of all the elements in the list: {sum(numbers)}")
