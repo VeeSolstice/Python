a40a75134b8a9ecbe65dc2d7c4bde9f7fbd7a710
VeeSolstice <vainikapanwarr.21042@gmail.com>
2026-01-17 15:14:28 +0530

    Add examples for list methods: extend, remove, and pop

---
+++ Module_5.py/List_Operations_2.py
@@ -0,0 +1,51 @@
+"""
+extend()
+remove()
+pop()    
+"""
+
+# extend
+fruits = ["Apple", "Mango", "Orange"]
+# fruits.append("Banana", "Grapes")
+# This above sntax throws an error because append fn can add only one elent at a time not more
+# print(fruits)
+
+# fruits.append(["Banana", "Grapes"])
+# This will not throw an error because it considers everything inside the bracket as one. 
+# So, it will display list with one more list inside it
+# print(fruits)
+
+# fruits.extend("Banana" ,"Grapes")
+# This also gives TypeError
+# print(fruits)
+
+fruits.extend(["Banana","Grapes"])
+print(fruits)
+
+
+
+# Remove
+fruits = ["Apple", "Mango", "Orange"]
+print(fruits)
+fruits.remove("Mango")
+print(fruits)
+fruits = ["Apple", "Mango", "Orange", "Mango"]
+fruits.remove("Mango")
+# This fn removes first occurance incase of multiple occurances
+
+# fruits.remove("Banana")
+# This throws ValueError
+
+
+
+# pop
+fruits = ["Apple", "Mango", "Orange"]
+print(fruits)
+fruits.pop(2)
+print(fruits)
+
+fruits = ["Apple", "Mango", "Orange"]
+print(fruits)
+fruits.pop()
+# This bydefault removes the last element
+print(fruits)
