e5cf7c8c3269dea70c379d30fe9a66efab184681
VeeSolstice <vainikapanwarr.21042@gmail.com>
2026-01-25 18:59:40 +0530

    Add examples for tuple operations: concatenation and repetition

---
+++ Module_6.py/tuple_operations.py
@@ -0,0 +1,16 @@
+"""
+concatenation, repetition, membership
+count, index
+min, max, sum
+"""
+
+student_detail1 = (1001, "John")
+student_detail2 = (78.5, 91.0, 83.5, 79.5)
+
+# +
+student_details = student_detail1 + student_detail2
+print(student_details)
+
+# *
+t1 = ("Class 5", 5000)
+print(t1 * 3)
